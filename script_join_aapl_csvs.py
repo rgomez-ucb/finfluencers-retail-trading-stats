b7e21ee41701656295315f7e89364c1f8f930c9d
rgomez-ucb <jrgomez1231@berkeley.edu>
2025-11-28 12:22:17 -0800

    script to join APPL csvs 2020-2022

---
+++ script_join_aapl_csvs.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import pandas as pd
+
+data_directory = '/Users/ruben/Desktop/Applied-Stats-Prof-Max/Final_Project_Applied_Stats_Max_2025/StockTwits_2020_2022_Raw/AAPL_2020_2022/'
+
+pattern = os.path.join(data_directory, 'AAPL_*.csv')
+csv_files = sorted(glob.glob(pattern))
+
+print(f"Found {len(csv_files)} files")
+if not csv_files:
+    raise SystemExit("No CSV files found in the specified directory."
+                        )
+
+dataframes = []
+for i, file in enumerate(csv_files):
+    print(f"Reading file {i+1}/{len(csv_files)}: {os.path.basename(file)}")
+    df = pd.read_csv(file)
+    dataframes.append(df)
+
+full_df = pd.concat(dataframes, ignore_index=True)
+
+# save .csv of full merged file
+output = os.path.join(data_directory, 'AAPL_ALL_MERGED_2020_2022.csv')
+full_df.to_csv(output, index=False)
+
+print(f"\nDone. Saved merged file to:\n{output}")
+print(f"Final shape: {full_df.shape}")
